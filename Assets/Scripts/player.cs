4f1aab7f5dbc466e04f64ed148a66ceadbf0d8e8
micktsai <mick14758@gmail.com>
2021-11-03 17:41:53 +0800

    sth new
    better movement and bullet line and aim

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,13 +7,14 @@ public class player : MonoBehaviour
     bool w, a, s, d;
     bool click;
     float speed, mouse_x, mouse_y;
-    float rotate_apeed;
+    float rotate_apeed,jumptime;
     Camera mcam;
     public Vector3 velocity, mouseDir;
     Rigidbody rb;
     RaycastHit hit;
     public LayerMask mask;
     bool jump;
+    LineRenderer line;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,7 @@ public class player : MonoBehaviour
         speed = 10f;
         rotate_apeed = 2f;
         rb = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
         Cursor.lockState = CursorLockMode.Locked;
         jump = false;
     }
@@ -41,11 +43,12 @@ public class player : MonoBehaviour
             rb.velocity = new Vector3(0, 5.5f, 0);
         }
 
-        if(rb.velocity.y < 0)
+        if(jumptime <=0 )
         {
             jump = false;
+            jumptime = 2f;
         }
-
+        jumptime -= Time.deltaTime;
         mouse_x = Input.GetAxis("Mouse X");
         mouse_y = Input.GetAxis("Mouse Y");
 
@@ -64,14 +67,17 @@ public class player : MonoBehaviour
             velocity += this.transform.forward * speed * -1;
         if (d)
             velocity += this.transform.right * speed;
+        
         velocity.y = rb.velocity.y;
         rb.velocity = velocity;
 
         if (click)
         {
             click = false;
+            StartCoroutine(ShotEffect());
             if (Physics.Raycast(this.transform.position, mcam.transform.forward, out hit, 250f, mask))
             {
+                
                 Debug.Log(hit.transform.name);
             }
             //Debug.DrawLine(this.transform.position, this.transform.position + mcam.transform.forward * 100f);
@@ -80,5 +86,16 @@ public class player : MonoBehaviour
 
         this.transform.localEulerAngles = Vector3.up * mouseDir.y * rotate_apeed;
         mcam.transform.localEulerAngles = Vector3.right * mouseDir.x * rotate_apeed;
+    }
+    private IEnumerator ShotEffect()
+    {
+        line.SetPosition(0, this.transform.position);
+        line.SetPosition(1, this.transform.position + mcam.transform.forward * 250f);
+        Debug.Log("fire");
+        line.enabled = true;
+
+        yield return new WaitForSeconds(0.07f);
+
+        line.enabled = false;
     }
 }
