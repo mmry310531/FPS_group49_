d5ebd0de12ec57a62d5e1972ca3da4d81cb3369b
mmry310531 <46072832+mmry310531@users.noreply.github.com>
2021-11-05 12:50:55 +0800

    test

--- Assets/Scripts/BombGenerator.cs
+++ Assets/Scripts/BombGenerator.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class BombGenerator : MonoBehaviour
 {
+
     public GameObject bomb_prefab;
     public float Height;
+    public float timer;
+    public bool reload;
     // Update is called once per frame
+    private void Start()
+    {
+        reload = false;
+        timer = 1f;
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        timer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.E) && timer < 0f)
         {
             GameObject bomb = Instantiate<GameObject>(bomb_prefab);
             bomb.transform.position = transform.position + transform.forward + new Vector3( 0, 0.5f, 0 );
@@ -17,7 +26,8 @@ public class BombGenerator : MonoBehaviour
 
             Rigidbody rb = bomb.GetComponent<Rigidbody>();
             rb.AddForce(transform.forward*4 + new Vector3(0,Height,0), ForceMode.Impulse);;
-            
+            rb.velocity += transform.GetComponent<Rigidbody>().velocity;
+            timer = 1f;
         }
     }
 }
--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -8,19 +8,29 @@ public class Explode : MonoBehaviour
 {
     public float time;
     public float deltaTime;
-    float range = 10.0f;
+    float range = 5.0f;
     public Rigidbody rb;
     public GameObject ps;
+    SphereCollider sc;
+    public bool damage_bool;
+
+    void Start()
+    {
+        damage_bool = false;
+        sc = this.gameObject.GetComponent<SphereCollider>();
+    }
 
     void Update()
     {
         
         time -= Time.deltaTime * deltaTime;
-        if (time <= 0)
+        if (time <= 0 && !damage_bool)
         {
+            // sc.radius = 20;
             GOT_Explode();
             //rb.AddExplosionForce(200f, this.transform.position, 200f);
             GameObject ps_o = Instantiate(ps, transform.position, transform.rotation);
+            damage_bool = true;
             Destroy(this.gameObject);
             Destroy(ps_o, 3f);
         }
@@ -35,17 +45,17 @@ public class Explode : MonoBehaviour
         Collider[] colliders = Physics.OverlapSphere(transform.position, range);
         foreach(Collider c in colliders)
         {
-            Debug.Log(c.name);
+            //Debug.Log(c.name);
             if (c.gameObject.tag == "Enemy")
             {
-                GameObject.Find(c.name).GetComponent<HP>().hpnow -= 40;
-                // Damage(c.transform);
+                
+                Damage(c.transform);
             }
         }
     }
     void Damage(Transform transform)
     {
-        // TODO
+        transform.GetComponent<HP>().hpnow -= 40;
     }
 
 
